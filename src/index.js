7a38540f5a1a161cac575b2ff625782e6c913bbd
smooshi <smooshi@users.noreply.github.com>
2018-02-03 00:34:00 +0200

    Heroku 2

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ app.use(cors())
 app.use(bodyParser.json())
 morgan.token('json', function(req, res){ return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
+app.use(express.static('build'))
+
 
 let persons = [
     {
